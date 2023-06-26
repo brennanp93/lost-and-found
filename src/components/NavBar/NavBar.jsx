4b8b0a227ec5668a2bd5ce4c2e68cde4994721bb
brennanp93 <brennan.p.perez@gmail.com>
2023-06-26 09:42:22 -0700

    update navbar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,23 +7,15 @@ export default function NavBar({ user, setUser }) {
     setUser(null);
   }
 
-  return (
-    <>
-      <header>
-        <nav>
-          <Link to="/locations">Beaches</Link>
-          &nbsp; | &nbsp;
-          <Link to="/items/new">Post a lost or found item</Link>
-          <p></p>
-          &nbsp;&nbsp;
-          <span>Welcome, {user.name}</span>
-          &nbsp;&nbsp;
-          <Link to="" onClick={handleLogOut}>
-            Log Out
-          </Link>
-        </nav>
-        {/* <h4>Lost And Found</h4> */}
-      </header>
-    </>
+ return (
+    <header>
+      <h2>Lost And Found</h2>
+      <nav className="header-links">
+        <Link to="/locations">Beaches</Link>
+        <Link to="/items/new">Post item</Link>
+        <span>Welcome, {user.name}</span>
+        <button onClick={handleLogOut}>Log Out</button>
+      </nav>
+    </header>
   );
-}
+};
